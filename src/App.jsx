34932846e553d0c10a1a72e9dfc8b78ef672bf8e
syarifsakur <107300773+syarifsakur@users.noreply.github.com>
2024-04-23 02:40:33 +0800

    set up projek

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div>
+        <h1>tes</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="text-4xl font-bold bg-blue-600 h-20 flex items-center justify-center text-white shadow-xl poppins">
+        <h1>Portofolio</h1>
+    </div>
+
+  )
+}
+
+export default Navbar
